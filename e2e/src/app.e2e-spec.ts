7c6cf6f65f1a135083f4cb48216b5f60c148a0ee
Liz Kalter <liz.kalter@gmail.com>
2019-05-25 22:17:34 -0400

    rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-quiz2!');
+    expect(page.getTitleText()).toEqual('Mad Scientist Personality Test');
   });
 
   afterEach(async () => {
